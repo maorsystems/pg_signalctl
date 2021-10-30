a5541e01c0e9a9f7300d6a87a2b1855c4b5059e3
Doron Yaary <93372115+maorsystems@users.noreply.github.com>
2021-10-30 20:51:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ echo " Detected OS: $OSNAME"
 if [ ! -d "/usr/pgsql-10/bin" ] && [ ! -d "/usr/pgsql-11/bin" ] && [ ! -d "/usr/pgsql-12/bin" ] && [ ! -d "/usr/pgsql-13/bin" ] && [ ! -d "/usr/pgsql-14/bin" ]; then
   echo "ERROR: Could not locate any PostgreSQL installation."
   echo ""
-  exit(0)
+  exit 0
 fi
