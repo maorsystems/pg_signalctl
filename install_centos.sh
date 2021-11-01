2366b62e950dd7140e1da80138c8dee49d17852b
Doron Yaary <93372115+maorsystems@users.noreply.github.com>
2021-11-01 10:03:37 +0200

    Update install_centos.sh

--- install_centos.sh
+++ install_centos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 PGSIGNAL_PGVERSION=""
 echo ""
 echo " PG_SIGNALCTL Installation @ CentOS/RHEL"
@@ -22,4 +23,25 @@ if [ -d "/usr/pgsql-13/bin" ]; then
 fi
 if [ -d "/usr/pgsql-14/bin" ]; then
   PGSIGNAL_PGVERSION="14"
+fi
+if [ "$PGSIGNAL_PGVERSION" == "" ]; then
+  echo " ERROR: Could not locate any PostgreSQL installation."
+  echo ""
+  exit 0
+fi
+CHK=$(which python3)
+if [ $? -ne 0 ]; then
+  echo " WARNING: Could not find Python3 executable. Trying to install it automatically..."
+  yum install -y python3
+fi
+CHK=$(which pip3)
+if [ $? -ne 0 ]; then
+  echo " WARNING: Could not find Python3 installer executable. Trying to install it automatically..."
+  yum install -y python3-pip
+  exit 0
+fi
+CHK=$(pip3 list --format=columns | grep psycopg2 | grep psycopg2 | wc -l)
+if [ "$CHK" != "1" ]; then
+  echo " WARNING: Could not find the psycopg2 python package. Trying to install it automatically..."
+  yum install -y python3-psycopg2
 fi
