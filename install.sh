c4c769680f1c4b4e6d9d6d7743fa8d7710f57e37
Doron Yaary <93372115+maorsystems@users.noreply.github.com>
2021-11-01 09:47:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,17 @@ PGSIGNAL_PGVERSION=""
 echo ""
 echo " PG_SIGNALCTL Installation"
 echo ""
-OSNAME=$(cat /etc/os-release | egrep '^NAME="' | sed 's/NAME="//g' | sed 's/"//g' | xargs)
+OSNAME=""
+TMP=$(cat /etc/os-release | egrep '^NAME="' | sed 's/NAME="//g' | sed 's/"//g' | tr '[:upper:]' '[:lower:]' | xargs | grep centos | wc -l)
+if [ "$TMP" == "1" ]; then
+  OSNAME="centos"
+fi
+if [ "$OSNAME" == "" ]; then
+  TMP=$(cat /etc/os-release | egrep '^NAME="' | sed 's/NAME="//g' | sed 's/"//g' | tr '[:upper:]' '[:lower:]' | xargs | grep ubuntu | wc -l)
+  if [ "$TMP" == "1" ]; then
+    OSNAME="ubuntu"
+  fi
+fi
 echo " Detected OS: $OSNAME"
 if [ ! -d "/usr/pgsql-10/bin" ] && [ ! -d "/usr/pgsql-11/bin" ] && [ ! -d "/usr/pgsql-12/bin" ] && [ ! -d "/usr/pgsql-13/bin" ] && [ ! -d "/usr/pgsql-14/bin" ]; then
   echo " ERROR: Could not locate any PostgreSQL installation."
